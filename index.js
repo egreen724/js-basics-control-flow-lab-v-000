79b0a31defdef1367eefda88d27ae0fc975511f6
Erin Green <erin.y.green@gmail.com>
2019-04-30 20:05:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function scuberGreetingForFeet(feet) {
   let message;
 
   if (feet <= 400) {
-    message = 'This one is on me!'
+    message = 'This one is on me!';
   } else if (feet > 2000) {
     message = 'I will gladly take your thirty bucks.'
   } else if (feet > 2500) {
     message = 'No can do.'
   }
 
-  message; 
+  message;
 }
 
 function ternaryCheckCity() {
