a57331ed3b8eccbb51d763dd76d93e4f1ad7cd58
Erin Green <erin.y.green@gmail.com>
2019-04-30 20:02:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // Write your code in this file!
-function scuberGreetingForFeet() {
+function scuberGreetingForFeet(feet) {
+  let message;
 
-
+  if (feet <= 400) {
+    message = 'This one is on me!'
+  } else if (feet > 2000) {
+    message = 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    'No can do.'
+  }
 }
+
+it('charges 30 dollars for a distance over 2000 feet', function () {
+  expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
+});
+
+it('does not allow rides over 2500 feet', function () {
+  expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
+});
 
 function ternaryCheckCity() {
 
@@ -11,5 +26,5 @@ function ternaryCheckCity() {
 
 function switchOnCharmFromTip() {
 
-  
+
 }
