74217b1652989daff056b0b252c4d75e47e43291
Erin Green <erin.y.green@gmail.com>
2019-04-30 20:02:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ function scuberGreetingForFeet(feet) {
   }
 }
 
-it('charges 30 dollars for a distance over 2000 feet', function () {
-  expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
-});
-
-it('does not allow rides over 2500 feet', function () {
-  expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
-});
-
 function ternaryCheckCity() {
 
 
