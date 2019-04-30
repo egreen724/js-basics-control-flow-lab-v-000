73ee40eb4c6634bd15e838da85a3d42db2e735ce
Erin Green <erin.y.green@gmail.com>
2019-04-30 20:19:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,28 @@ function ternaryCheckCity(city) {
   return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip() {
+function switchOnCharmFromTip(tip) {
 
+  switch(tip) {
+    case "generous":
+      return "Thank you so much."; 
+    case "not as generous":
+      return "Thank you."
+    break 
+    default: 
+      return "Bye."
+  }
 
 }
+
+it('should return "Thank you so much." if the tip is generous', function () {
+  expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+});
+
+it('should return "Thank you." if the tip is not as generous', function () {
+  expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+});
+
+it('should return "Bye." if anything else', function () {
+  expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+});
